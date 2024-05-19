d9cd50daa0336fb437bf2bdf6e42de904aafe587
ZoeAcero <zar.amazona@gmail.com>
2024-05-19 11:11:15 +0200

    a

--- src/main/java/GestorDeArchivos/GestorDeArchivos.java
+++ src/main/java/GestorDeArchivos/GestorDeArchivos.java
@@ -8,4 +8,16 @@ import java.nio.file.Paths;
 import java.util.stream.Stream;
 
 public class GestorDeArchivos {
+
+    public static void guardarExperimento(Experimento experimento, String rutaArchivo) {
+        try {
+            FileOutputStream fileOut = new FileOutputStream(rutaArchivo);
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(experimento);
+            out.close();
+            fileOut.close();
+        } catch (IOException i) {
+            i.printStackTrace();
+        }
+    }
 }
