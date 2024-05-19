2bbdedee9dc987bd1c5710357b07da0ccd510ae6
ZoeAcero <zar.amazona@gmail.com>
2024-05-19 11:10:49 +0200

    a

---
+++ src/main/java/GestorDeArchivos/GestorDeArchivos.java
@@ -0,0 +1,11 @@
+package GestorDeArchivos;
+
+import Experimento.Experimento;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class GestorDeArchivos {
+}
