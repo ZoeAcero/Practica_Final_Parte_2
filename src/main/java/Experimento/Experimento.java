5fc43ab66f116f4f06127a0d4d2edeb068521ca1
ZoeAcero <zar.amazona@gmail.com>
2024-05-19 11:00:04 +0200

    a

--- src/main/java/Experimento/Experimento.java
+++ src/main/java/Experimento/Experimento.java
@@ -1,4 +1,11 @@
 package Experimento;
 
+import PoblacionDeBacterias.Poblacion;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+
 public class Experimento {
+    private List<Poblacion> poblaciones;
+    private int duracion;
 }
