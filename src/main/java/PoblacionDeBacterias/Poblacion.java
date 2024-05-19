29c7e6878901cd51b78a72d53201d7a486330a08
ZoeAcero <zar.amazona@gmail.com>
2024-05-19 11:25:37 +0200

    a

--- src/main/java/PoblacionDeBacterias/Poblacion.java
+++ src/main/java/PoblacionDeBacterias/Poblacion.java
@@ -2,15 +2,15 @@ package PoblacionDeBacterias;
 
 import Experimento.Bacteria;
 import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
-import java.util.Objects;
 
 public class Poblacion {
     private String nombre;
     private LocalDate fechaInicio;
     private List<Bacteria> bacterias;
 
-    public Poblacion(String nombre, LocalDate fechaInicio, List<Bacteria> bacterias) {
+    public Poblacion(String nombre, Date fechaInicio, List<Bacteria> bacterias) {
         setNombre(nombre);
         setFechaInicio(fechaInicio);
         setBacterias(bacterias);
