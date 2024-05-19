afb71603aa2c1ee36163fadd2812d3711c1e60c3
ZoeAcero <zar.amazona@gmail.com>
2024-05-19 10:52:36 +0200

    a

--- src/main/java/Experimento/Bacteria.java
+++ src/main/java/Experimento/Bacteria.java
@@ -1,4 +1,15 @@
 package Experimento;
 
+import java.util.Random;
+
 public class Bacteria {
+
+    private static final int UMBRAL_ALTO = 150000;
+    private static final int UMBRAL_MEDIO = 100000;
+    private static final int UMBRAL_BAJO = 50000;
+
+    private int x;
+    private int y;
+    private int comidaConsumida;
+    private Random random;
 }
