a1eec01cb152a0eb94c9d6e57384738acbd43042
ZoeAcero <zar.amazona@gmail.com>
2024-05-19 11:14:48 +0200

    a

--- src/main/java/Simulacion/Simulacion.java
+++ src/main/java/Simulacion/Simulacion.java
@@ -5,5 +5,16 @@ import Experimento.Plato;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+
 public class Simulacion {
+    private Plato plato;
+    private List<Bacteria> bacterias;
+    private Random random;
+
+    public Simulacion() {
+        this.plato = new Plato(20);
+        this.bacterias = new ArrayList<>();
+        this.random = new Random();
+        inicializarBacterias();
+    }
 }
