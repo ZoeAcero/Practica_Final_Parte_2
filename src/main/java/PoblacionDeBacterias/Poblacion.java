0aebde6f1eb385bd1e620aba527d0adc5dd43f41
ZoeAcero <zar.amazona@gmail.com>
2024-05-19 10:57:09 +0200

    a

---
+++ src/main/java/PoblacionDeBacterias/Poblacion.java
@@ -0,0 +1,13 @@
+package PoblacionDeBacterias;
+
+import Experimento.Bacteria;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Objects;
+
+public class Poblacion {
+    private String nombre;
+    private LocalDate fechaInicio;
+    private List<Bacteria> bacterias;
+
+}
