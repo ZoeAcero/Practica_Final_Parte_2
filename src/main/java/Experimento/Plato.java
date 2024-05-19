c1ff33bc5c9de90d9e247e9ddd66b5a5c3e1c0b7
ZoeAcero <zar.amazona@gmail.com>
2024-05-19 11:05:32 +0200

    a

--- src/main/java/Experimento/Plato.java
+++ src/main/java/Experimento/Plato.java
@@ -7,4 +7,18 @@ public class Plato {
 
     private int[][] celdas;
     private Random random;
+
+    public Plato(int size) {
+        if (size <= 0) {
+            throw new IllegalArgumentException("El tamaño del plato debe ser positivo");
+        }
+        this.celdas = new int[size][size];
+        this.random = new Random();
+        inicializar();
+    }
+
+    public int getCelda(int x, int y) {
+        validarCoordenadas(x, y);
+        return celdas[x][y];
+    }
 }
