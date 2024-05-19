ce99b84dca485ebb9922b387cf3c986e02f401e8
ZoeAcero <zar.amazona@gmail.com>
2024-05-19 11:14:29 +0200

    a

---
+++ src/main/java/Simulacion/Simulacion.java
@@ -0,0 +1,9 @@
+package Simulacion;
+
+import Experimento.Bacteria;
+import Experimento.Plato;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+public class Simulacion {
+}
