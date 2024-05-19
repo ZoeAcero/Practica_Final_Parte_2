491b554c0ca734a73bc99415b4bf8af89a1fe706
ZoeAcero <zar.amazona@gmail.com>
2024-05-19 11:05:08 +0200

    a

---
+++ src/main/java/Experimento/Plato.java
@@ -0,0 +1,10 @@
+package Experimento;
+
+import java.util.Random;
+
+public class Plato {
+    private static final int COMIDA_INICIAL = 20000;
+
+    private int[][] celdas;
+    private Random random;
+}
